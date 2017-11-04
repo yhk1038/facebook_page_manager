14737e81fcf34e87b37fc051ce30ced9eec25e59
Fred Kim <yhk1038@hanmail.net>
2017-11-04 22:12:56 +0900

    Add user model

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
     root 'site#index'
 
     devise_for :users, controllers: {
-        sessions: 'users/sessions'
+        sessions: 'users/sessions',
+        registrations: 'users/registrations',
+        omniauth_callbacks: 'users/omniauth_callbacks'
     }
 end
